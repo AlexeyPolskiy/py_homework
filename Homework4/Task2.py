c1e8ff5214aa35d6fb318b23f12c8144c5f7d21a
Alexey <alexey.polskiy@gmail.com>
2022-10-15 20:29:35 +0500

    homework4 Task2 completed

--- Homework4/Task2.py
+++ Homework4/Task2.py
@@ -4,5 +4,25 @@
 *Пример*
 
 - при N=236     ->        [2, 2, 59]
-
 """
+
+num_ = int(input("Введите не отрицательное целое число = "))
+
+numb_ = num_
+
+k = 2
+
+lst_ = []
+
+while k <= num_:
+    if num_ % k == 0:
+        lst_.append(k)
+        num_ = num_ // k
+        k = 2
+    else:
+        k += 1
+
+# print(type(num_))
+
+print(f'При N = {numb_}   ->    {lst_}')
+
