a084990a9ef02ae22ae44b77c823e39ce19d91d9
Alexey <alexey.polskiy@gmail.com>
2022-10-16 18:17:41 +0500

    homework4 Task4 completed

--- Homework4/Task4.py
+++ Homework4/Task4.py
@@ -5,3 +5,28 @@
 
 - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 """
+
+import random
+
+k = int(input("Введите коэфицент k: "))
+
+array_ = []
+
+for i in range(k + 1):
+    numb_rand = int(random.randint(0, 100))
+    if numb_rand == 0:
+        continue
+    elif numb_rand > 0:
+        if len(array_) > 0:
+            array_.append(" + ")
+    if k - i > 1:
+        array_.append((str(f'{abs(numb_rand)}x^{k - i}')))
+    elif k - i == 1:
+        array_.append((str(f'{abs(numb_rand)}x')))
+    else:
+        array_.append((str(abs(numb_rand))))
+
+array_.append(" = 0")
+
+with open('Task4.txt', 'a') as file:
+    file.write("".join(array_) + "\n")
